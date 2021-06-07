9682f08018ad343638dbdf44f81bf2aadc2fe0cb
Rohit1823 <rohitmachale1823@gmail.com>
2021-06-07 14:19:56 +0530

    [Rohit]ADD: Use database functions SUM, AVG, MIN, MAX, COUNT

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -30,3 +30,13 @@ alter table employee_payroll add gender char(1);
 select * from employee_payroll;
 update employee_payroll set gender='M' where name in ('Ramesh','Jack','Mark');
 update employee_payroll set gender='F' where id in (2);
+
+select sum(salary) from employee_payroll where gender='M' group by gender;
+select sum(salary) from employee_payroll  group by gender;
+select sum(salary) as 'totalSalary',gender from employee_payroll group by gender;
+select avg(salary) as 'averageSalary',gender from employee_payroll group by gender;
+select min(salary) as 'minimumSalary',gender from employee_payroll group by gender;
+select max(salary) as 'maximumSalary',gender from employee_payroll group by gender;
+
+select count(gender) as 'male' from employee_payroll where gender='M' 
+select count(gender) as 'female' from employee_payroll where gender='F' 
