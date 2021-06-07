4d55e95fb1ef423ef463750e30b56abe33ecf2c5
Rohit1823 <rohitmachale1823@gmail.com>
2021-06-07 12:28:08 +0530

    [Rohit]ADD: Create a Employee Payroll Table in the payroll service database

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -3,3 +3,11 @@ create database payroll_service16
 select * from sys.databases
 
 use payroll_service16
+
+create table employee_payroll
+(
+id int identity(1,1) primary key,
+name varchar(20) not null,
+salary money not null,
+start date not null,
+);
